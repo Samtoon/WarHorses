e4572fdd18a27230137c03564d8d36c36225a9b2
ErickAndrade0809 <62568886+ErickAndrade0809@users.noreply.github.com>
2023-01-17 13:02:39 -0500

    Add files via upload

---
+++ caballoIA.js
@@ -0,0 +1,212 @@
+import { Caballo } from "./caballo.js";
+import Nodo from "./nodo.js";
+import { Casilla, leerTablero, max, min } from "./tablero.js";
+import valores from "./valoresTablero.json" assert {type: "json"};
+
+class CaballoIA extends Caballo {
+    constructor(fila = 0, columna = 0, max = true, dificultad = 1) {
+        super(fila, columna, max);
+        this.dificultad = dificultad;
+        this.malPodados = 0;
+    }
+    decidirMovimiento() {
+        let podarHasta = -1;
+        this.ciclosEvitados = 0;
+        this.arbolMinimax = [];
+        
+        const movimientosPosibles = valores.movimientosPosibles;
+        this.arbolMinimax.push(new Nodo(0, 0, null, [], [], 0, this.max ? Number.NEGATIVE_INFINITY : Number.POSITIVE_INFINITY, this.max));
+        const nodosPendientes = [0];
+        let index = 1;
+        // while (nodosPendientes.length > 0) {
+        //     expandir(this.arbolMinimax, this.arbolMinimax[nodosPendientes[0]].profundidad < this.dificultad * 2 - 1 ? false : true);
+        //     nodosPendientes.shift();
+        // }
+        //let nodoFinal = null;
+        while (this.arbolMinimax.length > 1 || !this.arbolMinimax[0].terminal) {
+            while (!this.arbolMinimax[this.arbolMinimax.length - 1].terminal) {
+                expandir(this.arbolMinimax, this.arbolMinimax[this.arbolMinimax.length - 1].profundidad == this.dificultad * 2 - 1);
+            }
+            while (this.arbolMinimax[this.arbolMinimax.length - 1].terminal && this.arbolMinimax.length > 1) {
+                analizar(this.arbolMinimax[this.arbolMinimax.length - 1], this.dificultad);
+                this.arbolMinimax.pop();
+                while (podarHasta != -1) {
+                    if (this.arbolMinimax[this.arbolMinimax.length - 1].id == podarHasta) {
+                        podarHasta = -1;
+                    } else {
+                        if (!descendiente(this.arbolMinimax[this.arbolMinimax.length - 1], podarHasta)) {
+                            console.log("Yo era bueno, nooooooooo");
+                            this.malPodados++;
+                        }
+                        this.arbolMinimax.pop();
+                    }
+                }
+                // if (podarHasta != -1) {
+                //     let index = 0;
+                //     while (index < this.arbolMinimax.length) {
+                //         if (descendiente(this.arbolMinimax[this.arbolMinimax.length - 1], podarHasta)) {
+                //         this.arbolMinimax.pop();
+                //         }
+                //         index++;
+                //     }
+                //     podarHasta = -1;
+                // }
+
+            }
+        }
+        if (this.arbolMinimax[0].movimiento.length > 0) {
+            //console.log("Mi utilidad es de " + this.arbolMinimax[0].utilidad);
+            this.mover(this.arbolMinimax[0].movimiento[0], this.arbolMinimax[0].movimiento[1]);
+        } else {
+            this.bloqueado = true;
+        }
+        // if (this.arbolMinimax.length < 2) {
+        //     this.bloqueado = true;
+        // }
+        // else {
+        //     while (this.arbolMinimax[this.arbolMinimax.length - 1].profundidad != 1) {
+        //         analizar(this.arbolMinimax[this.arbolMinimax.length - 1], this.dificultad);
+        //         this.arbolMinimax.pop();
+        //     }
+        //     for (let i = this.arbolMinimax.length - 1; i > 0; i--) {
+        //         analizar(this.arbolMinimax[i]);
+        //     }
+        //     const utilidadFinal = this.arbolMinimax[0].utilidad;
+        //     //console.log("Mi utilidad es de " + utilidadFinal);
+        //     this.arbolMinimax.shift();
+
+        //     index = 0;
+        //     while (index < this.arbolMinimax.length) {
+        //         if (this.arbolMinimax[index].utilidad != utilidadFinal) {
+        //             this.arbolMinimax.splice(index, 1);
+        //         } else {
+        //             index++;
+        //         }
+        //     }
+        //     const nodoFinal = this.arbolMinimax[Math.floor(Math.random() * this.arbolMinimax.length)];
+        //     if (nodoFinal.movimiento.length > 0) {
+        //         this.mover(nodoFinal.movimiento[0], nodoFinal.movimiento[1]);
+        //     } else {
+        //         this.bloqueado = true;
+        //     }
+
+        //}
+        function expandir(arbolMinimax = [], terminales = true) {
+            const nodo = arbolMinimax[arbolMinimax.length - 1];
+            if (nodo.padre != null) {
+                nodo.alfa = nodo.padre.alfa;
+                nodo.beta = nodo.padre.beta;
+            }
+            let esteril = true;
+            let terminal = terminales;
+            const caballo = nodo.max ? max : min;
+
+            for (let i = 0; i < movimientosPosibles.length; i++) {
+                let nuevaFila = caballo.fila + movimientosPosibles[i][0];
+                let nuevaColumna = caballo.columna + movimientosPosibles[i][1];
+                if (nodo.profundidad > 1) {
+                    const abuelo = nodo.padre.casillasAfectadas[0];
+                    nuevaFila = abuelo.fila + movimientosPosibles[i][0];
+                    nuevaColumna = abuelo.columna + movimientosPosibles[i][1];
+                }
+                if (caballo.casillaDisponible(nuevaFila, nuevaColumna, nodo)) {
+                    let casillas = [new Casilla(nuevaFila, nuevaColumna, caballo.valores.id)];
+                    if (leerTablero(nuevaFila, nuevaColumna) == valores.bonificacion) {
+                        casillas = casillas.concat(caballo.efectoBonificacion(nuevaFila, nuevaColumna, nodo));
+                    }
+                    esteril = false;
+                    arbolMinimax.push(new Nodo(index, nodo.id, nodo, movimientosPosibles[i], casillas, nodo.profundidad + 1, -nodo.utilidad, !nodo.max, terminal, nodo.alfa, nodo.beta));
+                    if (!terminal) {
+                        nodosPendientes.push(index);
+                    }
+                    index++;
+                }
+            }
+            if (esteril) {
+                //console.log(`El nodo ${nodo.id} es estéril`);
+                const fila = nodo.profundidad > 1 ? nodo.padre.casillasAfectadas[0].fila : caballo.fila;
+                const columna = nodo.profundidad > 1 ? nodo.padre.casillasAfectadas[0].columna : caballo.columna;
+                terminal = nodo.esteril ? true : terminal;
+                arbolMinimax.push(new Nodo(index, nodo.id, nodo, [], [new Casilla(fila, columna, caballo.valores.id)], nodo.profundidad + 1, -nodo.utilidad, !nodo.max, terminal, nodo.alfa, nodo.beta));
+                if (!terminal) {
+                    nodosPendientes.push(index);
+                }
+                index++;
+            }
+            nodo.esteril = esteril;
+        }
+        function analizar(nodo = new Nodo(), dificultad) {
+            // if (nodo.padre != null) {
+            //     nodo.alfa = nodo.padre.alfa;
+            //     nodo.beta = nodo.padre.beta;
+            // }
+            if (!Number.isFinite(nodo.utilidad)) {
+                let pintadasMax = max.puntaje;
+                let pintadasMin = min.puntaje;
+                let nodoActual = nodo;
+                while (nodoActual.padre != null) {
+                    let valor = nodoActual.casillasAfectadas.length;
+                    if (nodoActual.padre.esteril) {
+                        valor -= 100;
+                    }
+                    if (nodoActual.max) {
+                        pintadasMin += valor;
+                    } else {
+                        pintadasMax += valor;
+                    }
+                    nodoActual = nodoActual.padre;
+                }
+                nodo.utilidad = pintadasMax - pintadasMin;
+
+            }
+            if (nodo.actualizarPadre()) {
+                podarHasta = nodo.padre.id;
+            }
+        }
+        function descendiente(nodo, id) {
+            let nodoActual = nodo;
+            while (nodoActual.padre != null) {
+                if (nodoActual.padre.id == id) {
+                    return true;
+                }
+                nodoActual = nodoActual.padre;
+            }
+            return false;
+        }
+        /* let index = 0
+        while (index < movimientosPosibles.length) {
+            if (this.casillaDisponible(this.fila + movimientosPosibles[index][0], this.columna + movimientosPosibles[index][1])){
+                index++;
+            } else {
+                movimientosPosibles.splice(index, 1);
+            }
+        }
+        if (movimientosPosibles.length > 0) {
+            const movimientoElegido = movimientosPosibles[Math.floor(Math.random() * movimientosPosibles.length)];
+            this.mover(movimientoElegido[0], movimientoElegido[1]);
+        } else {
+            this.bloqueado = true;
+        } */
+
+    }
+    casillaDisponible(fila, columna, nodo = new Nodo()) {
+        if (super.casillaDisponible(fila, columna)) {
+            let nodoActual = nodo.copiar();
+            while (nodoActual.padre != null) {
+                for (let i = 0; i < nodoActual.casillasAfectadas.length; i++) {
+                    if (nodoActual.casillasAfectadas[i].fila == fila &&
+                        nodoActual.casillasAfectadas[i].columna == columna) {
+                        this.ciclosEvitados++;
+                        return false;
+                    }
+                }
+                nodoActual = nodoActual.padre.copiar();
+            }
+            return true;
+        }
+        this.ciclosEvitados++;
+        return false;
+    }
+}
+
+export { CaballoIA }
---
+++ tablero.js
@@ -0,0 +1,208 @@
+import { Caballo } from "./caballo.js";
+import { CaballoIA } from "./caballoIA.js";
+import valores from "./valoresTablero.json" assert {type: "json"};
+import graficar from "./movimiento.js";
+
+class Casilla {
+    constructor(fila, columna, valor = 0) {
+        this.fila = fila;
+        this.columna = columna;
+        this.valor = valor;
+    }
+    pintarCasilla() {
+        tablero[this.fila][this.columna] = this.valor;
+    }
+}
+const tablero = [];
+const movDisponible=[];
+const filaMax = 2;
+const columnaMax = 2;
+const filaBono = 3;
+const columnaBono = 0;
+const nFilas = 8;
+const nColumnas = 8;
+let max;
+let min;
+
+function crearTablero() {
+    for (let j = 0; j < nFilas; j++) {
+        tablero.push([]);
+        for (let i = 0; i < nColumnas; i++) {
+            tablero[j].push(valores.vacio);
+        }
+    }
+    
+    function adyacente(fila, columna) {
+        const adyacentes = [[-1, -1,], [-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1], [0, -1]];
+        for (let i = 0; i < adyacentes.length; i++) {
+            const filaEvaluada = fila + adyacentes[i][0];
+            const columnaEvaluada = columna + adyacentes[i][1];
+            if (filaEvaluada >= 0 &&
+                filaEvaluada < nFilas &&
+                columnaEvaluada >= 0 &&
+                columnaEvaluada < nColumnas) {
+                    if (leerTablero(filaEvaluada, columnaEvaluada) != valores.vacio) {
+                        return true
+                    }
+                }
+        }
+        return false;
+    }
+    let fila = Math.floor(Math.random() * nFilas);
+    let columna = Math.floor(Math.random() * nColumnas);
+    for (let i = 0; i < 5; i++) {
+        while (leerTablero(fila, columna) != valores.vacio || adyacente(fila, columna)) {
+            fila = Math.floor(Math.random() * nFilas);
+            columna = Math.floor(Math.random() * nColumnas);
+        }
+        //console.log(`Fila elegida ${fila}, Columna elegida ${columna}`);
+        switch (i) {
+            case 0:
+                max = new CaballoIA(fila, columna, true, 1);
+                break;
+            case 1:
+                min = new Caballo(fila, columna, false);
+                break;
+            default:
+                tablero[fila][columna] = valores.bonificacion;
+                
+                //console.log("Bonificación en [" + columna + "," + fila + "]");
+        }
+    }
+}
+
+function pintarTablero(casillas) {
+    const nCasillas = casillas.length
+    for (let i = 0; i < nCasillas; i++) {
+        casillas[i].pintarCasilla();
+    }
+   
+}
+
+function tableroLleno() {
+    for (let j = 0; j < tablero.length; j++) {
+        for (let i = 0; i < tablero[0].length; i++) {
+            if (tablero[j][i] == valores.vacio) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+const leerTablero = (fila, columna) => tablero[fila][columna];
+
+function display() {
+    let puntajeMin = 0;
+    let puntajeMax = 0;
+    console.log("\n");
+    for (let j = 0; j < tablero.length; j++) {
+        let fila = "";
+        for (let i = 0; i < tablero[0].length; i++) {
+            fila += tablero[j][i] + "   ";
+            if (tablero[j][i] == max.valores.pintura) {
+                puntajeMax += 1;
+            } else if (tablero[j][i] == min.valores.pintura) {
+                puntajeMin += 1;
+            }
+        }
+         console.log(j + " | " + fila);
+    }
+     console.log("Puntaje Min: " + puntajeMin);
+     console.log("Puntaje Max:" + puntajeMax);
+     max.puntaje = puntajeMax;
+     min.puntaje = puntajeMin;
+     presentarPuntajes(puntajeMax+1,puntajeMin+1);
+     graficar(tablero)
+    return puntajeMax - puntajeMin;
+    
+}
+
+
+crearTablero();
+console.log("gola");
+graficar(tablero);
+console.log("gola");
+display();
+
+
+let victoriasMax = 0;
+let victoriasMin = 0;
+let empates = 0;
+const date1 = new Date();
+
+function iniciarjuego(dificultad) {
+
+    /*switch(dificultad){
+        case 1:
+            document.getElementById("facil").classList.add("oprimido")
+            break;
+        case 2:
+            document.getElementById("medio").classList.add("oprimido")
+            break;
+        case 3:
+            document.getElementById("dificil").classList.add("oprimido")
+            break;
+    }*/
+       
+
+    
+    max.dificultad = dificultad; 
+        display()
+        graficar(tablero);
+        max.decidirMovimiento();
+        graficar(tablero);
+        display()
+        min.añadirListeners();   
+}
+
+function jugar(){
+    display()
+        graficar(tablero);
+        max.decidirMovimiento();
+        graficar(tablero);
+        display()
+        min.añadirListeners();  
+}
+
+function presentarPuntajes(puntajeRojo,puntajeAmarillo){
+    document.getElementById("pr").innerHTML="<h2 class='lines-effect'>Casillas Rojas:"+puntajeRojo+"</h2>";
+    document.getElementById("pa").innerHTML="<h2 class='lines-effect'>Casillas Amarillas:"+puntajeAmarillo+"</h2>";;
+    let libres=64-(puntajeRojo+puntajeAmarillo);
+    document.getElementById("pl").innerHTML="<h2 class='lines-effect'>Casillas Libres:"+libres+"</h2>";;
+}
+
+
+
+
+// for (let i = 1; i <= 1000; i++) {
+//     while (!max.bloqueado || !min.bloqueado) {
+//     max.decidirMovimiento();
+//     display();
+//     min.decidirMovimiento();
+//     display();
+//     }
+//     const resultado = display();
+//     if (resultado > 0) {
+//     victoriasMax++;
+//     } else if (resultado < 0) {
+//     victoriasMin++;
+//     } else {
+//     empates++;
+//     }
+//     while (tablero.length > 0) {
+//         tablero.pop();
+//     }
+//     crearTablero();
+//     console.log(`Ganadas Max: ${victoriasMax/i}, Ganadas Min: ${victoriasMin/i}, Empates: ${empates/i}, No Partidas: ${i}, mal podados: ${max.malPodados + min.malPodados}`);
+
+// }
+const date2 = new Date();
+const diff = date2 - date1; //milliseconds interval
+console.log("Tardé " + diff + " milisegundos." );
+
+document.getElementById("facil").addEventListener("click",()=>iniciarjuego(1));
+document.getElementById("medio").addEventListener("click",()=>iniciarjuego(2));
+document.getElementById("dificil").addEventListener("click",()=>iniciarjuego(3));
+
+export { crearTablero, pintarTablero, leerTablero, nFilas, nColumnas, display, Casilla, tableroLleno, max, min, iniciarjuego,jugar };
